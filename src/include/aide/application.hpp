5c217dee7eb9bf582f32816859552b9b23df83a5
Markus Reitboeck <mrpilot@gmx.net>
2020-03-28 17:09:21 +0100

    (+) add minimum library and code
    test clang-format and static analyzers

---
+++ src/include/aide/application.hpp
@@ -0,0 +1,12 @@
+
+#ifndef AIDE_APPLICATION_HPP
+#define AIDE_APPLICATION_HPP
+
+namespace aide
+{
+    class Application
+    {
+        Application();
+    };
+} // namespace aide
+#endif // AIDE_APPLICATION_HPP
