6ac1599beb0897dda0b8e8f79d115f9a738e313a
Markus Reitboeck <mrpilot@gmx.net>
2020-03-28 21:14:55 +0100

    (+) add unit test for buildinformation

---
+++ tests/ut/buildinformationtest.cpp
@@ -0,0 +1,30 @@
+#include <string>
+
+#include <aide/buildinformation.hpp>
+#include <catch2/catch.hpp>
+
+using aide::build_information::GIT_HASH;
+
+TEST_CASE("Test version number check")
+{
+    static_assert(AIDE_VERSION >= AIDE_VERSION_CHECK(0, 0, 0),
+                  "Version should always be greater than 0.0.0");
+
+    REQUIRE(AIDE_VERSION >= AIDE_VERSION_CHECK(0, 0, 0));
+}
+
+TEST_CASE("Git Hash")
+{
+    SECTION("shall not be empty")
+    {
+        const std::string hash{GIT_HASH};
+        REQUIRE(!hash.empty());
+    }
+
+    SECTION("shall not contain NOTFOUND")
+    {
+        const std::string hash{GIT_HASH};
+        const size_t res{hash.find("NOTFOUND")};
+        REQUIRE(res == std::string::npos);
+    }
+}
