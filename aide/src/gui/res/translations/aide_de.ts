5c9f0c51ad5e79bb15a2587eb2d7643adad76bae
Markus Reitboeck <mrpilot@gmx.net>
2020-05-30 16:15:54 +0200

    (-) do not store source file location in ts files
    to avoid problems with generated files in build directories.
    if the build directory changes also the path in the *.ts file
    would change

--- aide/src/gui/res/translations/aide_de.ts
+++ aide/src/gui/res/translations/aide_de.ts
@@ -4,22 +4,17 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../forms/mainwindow.ui" line="14"/>
-        <location filename="../../../../../cmake-build-debug-clang/aide/src/gui/AideGui_autogen/include/ui_mainwindow.h" line="64"/>
         <source>MainWindow</source>
         <translation>Hauptfenster</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="28"/>
-        <location filename="../../../../../cmake-build-debug-clang/aide/src/gui/AideGui_autogen/include/ui_mainwindow.h" line="66"/>
         <source>File</source>
         <translation>Datei</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="40"/>
-        <location filename="../../../../../cmake-build-debug-clang/aide/src/gui/AideGui_autogen/include/ui_mainwindow.h" line="65"/>
         <source>Quit</source>
         <translation>Beenden</translation>
     </message>
 </context>
 </TS>
+    
--- aide/src/gui/res/translations/aide_en.ts
+++ aide/src/gui/res/translations/aide_en.ts
@@ -4,20 +4,14 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../forms/mainwindow.ui" line="14"/>
-        <location filename="../../../../../cmake-build-debug-clang/aide/src/gui/AideGui_autogen/include/ui_mainwindow.h" line="64"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="28"/>
-        <location filename="../../../../../cmake-build-debug-clang/aide/src/gui/AideGui_autogen/include/ui_mainwindow.h" line="66"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="40"/>
-        <location filename="../../../../../cmake-build-debug-clang/aide/src/gui/AideGui_autogen/include/ui_mainwindow.h" line="65"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
