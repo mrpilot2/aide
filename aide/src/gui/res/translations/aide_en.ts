d74f2463f87d0158245f578ff39c253d7003d7ca
Markus Reitboeck <mrpilot@gmx.net>
2020-06-23 22:10:17 +0200

    (=) :lipstick: add english language again
    english translation might be required for plurals
    reverts commit c24e1320

---
+++ aide/src/gui/res/translations/aide_en.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
