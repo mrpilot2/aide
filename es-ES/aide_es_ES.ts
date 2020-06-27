69a90990f86b4680ef001f0b65796c98a4a4a1da
Markus Reitboeck <mrpilot@gmx.net>
2020-06-27 17:52:12 +0200

    New translations aide_en.ts (Spanish)

---
+++ es-ES/aide_es_ES.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es-ES" sourcelanguage="en">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <source>MainWindow</source>
+      <translation type="unfinished">MainWindow</translation>
+    </message>
+    <message>
+      <source>File</source>
+      <translation type="unfinished">File</translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation type="unfinished">Quit</translation>
+    </message>
+  </context>
+</TS>
