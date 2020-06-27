947929642f08f2418990c8aba59d1cc272f7c5da
Markus Reitboeck <mrpilot@gmx.net>
2020-06-27 18:06:37 +0200

    New translations aide_en.ts (German)

---
+++ aide_de.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <source>MainWindow</source>
+      <translation>Hauptfenster</translation>
+    </message>
+    <message>
+      <source>File</source>
+      <translation>Datei</translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation>Beenden</translation>
+    </message>
+  </context>
+</TS>
