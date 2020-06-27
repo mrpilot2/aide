2c6b52dee2ee8dd38fbf7cc944ac9946abecdf6d
Markus Reitboeck <mrpilot@gmx.net>
2020-06-27 19:12:09 +0200

    New translations aide_en.ts (Hebrew) [ci skip]

---
+++ aide/src/gui/res/translations/aide_he.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="he" sourcelanguage="en">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <source>MainWindow</source>
+      <translation type="unfinished">MainWindow</translation>
+    </message>
+    <message>
+      <source>File</source>
+      <translation type="unfinished">File</translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation type="unfinished">Quit</translation>
+    </message>
+    <message>
+      <source>Help</source>
+      <translation type="unfinished">Help</translation>
+    </message>
+    <message>
+      <source>About Qt</source>
+      <translation type="unfinished">About Qt</translation>
+    </message>
+    <message>
+      <source>About aIDE</source>
+      <translation type="unfinished">About aIDE</translation>
+    </message>
+  </context>
+</TS>
