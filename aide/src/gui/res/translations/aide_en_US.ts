337bfa2c6472652bf8fb3c1d2bd5688cbbd45f36
Markus Reitboeck <mrpilot@gmx.net>
2020-11-01 13:56:30 +0100

    New translations aide_en.ts (English) [ci skip]

---
+++ aide/src/gui/res/translations/aide_en_US.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+  <context>
+    <name>FindActionDialog</name>
+    <message>
+      <source>Dialog</source>
+      <translation type="unfinished">Dialog</translation>
+    </message>
+    <message>
+      <source>Actions</source>
+      <translation type="unfinished">Actions</translation>
+    </message>
+    <message>
+      <source>Press Alt Enter</source>
+      <translation type="unfinished">Press Alt Enter</translation>
+    </message>
+    <message>
+      <source>Page</source>
+      <translation type="unfinished">Page</translation>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <source>MainWindow</source>
+      <translation type="unfinished">MainWindow</translation>
+    </message>
+    <message>
+      <source>File</source>
+      <translation type="unfinished">File</translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation type="unfinished">Quit</translation>
+    </message>
+    <message>
+      <source>Help</source>
+      <translation type="unfinished">Help</translation>
+    </message>
+    <message>
+      <source>About Qt</source>
+      <translation type="unfinished">About Qt</translation>
+    </message>
+    <message>
+      <source>About aIDE</source>
+      <translation type="unfinished">About aIDE</translation>
+    </message>
+  </context>
+  <context>
+    <name>QApplication</name>
+    <message>
+      <source>Quits the application</source>
+      <translation type="unfinished">Quits the application</translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation type="unfinished">Quit</translation>
+    </message>
+    <message>
+      <source>About Qt</source>
+      <translation type="unfinished">About Qt</translation>
+    </message>
+  </context>
+  <context>
+    <name>SearchLineEdit</name>
+    <message>
+      <source>Form</source>
+      <translation type="unfinished">Form</translation>
+    </message>
+  </context>
+</TS>
