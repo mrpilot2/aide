3cdf1c1c9cfa234993cd3adbf562c975d9dd19de
Markus Reitboeck <mrpilot@gmx.net>
2020-11-01 13:56:57 +0100

    New translations aide_en.ts (German) [ci skip]

--- aide/src/gui/res/translations/aide_de_DE.ts
+++ aide/src/gui/res/translations/aide_de_DE.ts
@@ -1,72 +1,72 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de" sourcelanguage="en">
-<context>
+  <context>
     <name>FindActionDialog</name>
     <message>
-        <source>Dialog</source>
-        <translation type="unfinished"></translation>
+      <source>Dialog</source>
+      <translation type="unfinished">Dialog</translation>
     </message>
     <message>
-        <source>Actions</source>
-        <translation type="unfinished"></translation>
+      <source>Actions</source>
+      <translation type="unfinished">Actions</translation>
     </message>
     <message>
-        <source>Press Alt Enter</source>
-        <translation type="unfinished"></translation>
+      <source>Press Alt Enter</source>
+      <translation type="unfinished">Press Alt Enter</translation>
     </message>
     <message>
-        <source>Page</source>
-        <translation type="unfinished"></translation>
+      <source>Page</source>
+      <translation type="unfinished">Page</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MainWindow</name>
     <message>
-        <source>MainWindow</source>
-        <translation>Hauptfenster</translation>
+      <source>MainWindow</source>
+      <translation>Hauptfenster</translation>
     </message>
     <message>
-        <source>File</source>
-        <translation>Datei</translation>
+      <source>File</source>
+      <translation>Datei</translation>
     </message>
     <message>
-        <source>Quit</source>
-        <translation>Beenden</translation>
+      <source>Quit</source>
+      <translation>Beenden</translation>
     </message>
     <message>
-        <source>Help</source>
-        <translation>Hilfe</translation>
+      <source>Help</source>
+      <translation>Hilfe</translation>
     </message>
     <message>
-        <source>About Qt</source>
-        <translation>Über Qt</translation>
+      <source>About Qt</source>
+      <translation>Über Qt</translation>
     </message>
     <message>
-        <source>About aIDE</source>
-        <translation>Über aIDE</translation>
+      <source>About aIDE</source>
+      <translation>Über aIDE</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>QApplication</name>
     <message>
-        <source>Quits the application</source>
-        <translation type="unfinished"></translation>
+      <source>Quits the application</source>
+      <translation type="unfinished">Quits the application</translation>
     </message>
     <message>
-        <source>Quit</source>
-        <translation type="unfinished">Beenden</translation>
+      <source>Quit</source>
+      <translation>Beenden</translation>
     </message>
     <message>
-        <source>About Qt</source>
-        <translation type="unfinished">Über Qt</translation>
+      <source>About Qt</source>
+      <translation>Über Qt</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SearchLineEdit</name>
     <message>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
+      <source>Form</source>
+      <translation type="unfinished">Form</translation>
     </message>
-</context>
+  </context>
 </TS>
