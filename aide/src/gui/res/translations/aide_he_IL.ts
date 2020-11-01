61220b376766ec4927eee1f566aeefd35b13926c
Markus Reitboeck <mrpilot@gmx.net>
2020-11-01 13:56:53 +0100

    New translations aide_en.ts (Hebrew) [ci skip]

--- aide/src/gui/res/translations/aide_he_IL.ts
+++ aide/src/gui/res/translations/aide_he_IL.ts
@@ -2,6 +2,25 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="he" sourcelanguage="en">
   <context>
+    <name>FindActionDialog</name>
+    <message>
+      <source>Dialog</source>
+      <translation type="unfinished">Dialog</translation>
+    </message>
+    <message>
+      <source>Actions</source>
+      <translation type="unfinished">Actions</translation>
+    </message>
+    <message>
+      <source>Press Alt Enter</source>
+      <translation type="unfinished">Press Alt Enter</translation>
+    </message>
+    <message>
+      <source>Page</source>
+      <translation type="unfinished">Page</translation>
+    </message>
+  </context>
+  <context>
     <name>MainWindow</name>
     <message>
       <source>MainWindow</source>
@@ -26,6 +45,28 @@
     <message>
       <source>About aIDE</source>
       <translation type="unfinished">About aIDE</translation>
+    </message>
+  </context>
+  <context>
+    <name>QApplication</name>
+    <message>
+      <source>Quits the application</source>
+      <translation type="unfinished">Quits the application</translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation type="unfinished">Quit</translation>
+    </message>
+    <message>
+      <source>About Qt</source>
+      <translation type="unfinished">About Qt</translation>
+    </message>
+  </context>
+  <context>
+    <name>SearchLineEdit</name>
+    <message>
+      <source>Form</source>
+      <translation type="unfinished">Form</translation>
     </message>
   </context>
 </TS>
