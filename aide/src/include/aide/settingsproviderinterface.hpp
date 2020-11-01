b6446069e54f32fd3e6ef4cb2a23e3f4257a08c1
Markus Reitboeck <mrpilot@gmx.net>
2020-11-01 17:17:52 +0100

    (=) :bulb: document SettingsProviderInterface

--- aide/src/include/aide/settingsproviderinterface.hpp
+++ aide/src/include/aide/settingsproviderinterface.hpp
@@ -10,9 +10,38 @@ namespace aide
     public:
         virtual ~SettingsProviderInterface();
 
+        /**
+         * use this for settings that can be stored in a version control
+         * system. If the implementation of SettingsInterface is file
+         * based, this creates a separate file to be stored in version control.
+         *
+         * The user shall be able to easily transfer application settings
+         * onto another computer.
+         *
+         * A setting that can be stored in version control is for example
+         * the selected application language.
+         * A setting that should not be stored in version control is for example
+         * the window geometry, as it might change with every application exit.
+         * @return current implementation of SettingsInterface
+         */
         [[nodiscard]] virtual SettingsInterface& versionableSettings()
             const = 0;
 
+        /**
+         * use this for settings that should not be stored in a version control
+         * system. If the implementation of SettingsInterface is file
+         * based, this creates a separate file for settings to be stored
+         * in version control.
+         *
+         * The user shall be able to easily transfer application settings
+         * onto another computer.
+         *
+         * A setting that can be stored in version control is for example
+         * the selected application language.
+         * A setting that should not be stored in version control is for example
+         * the window geometry, as it might change with every application exit.
+         * @return current implementation of SettingsInterface
+         */
         [[nodiscard]] virtual SettingsInterface& unversionableSettings()
             const = 0;
     };
