61ab1fc07385d62e732fd60e968b8d9e40fcb976
Markus Reitboeck <mrpilot@gmx.net>
2020-11-01 17:49:58 +0100

    (+) minimal SettingsInterface

--- aide/src/include/aide/settingsinterface.hpp
+++ aide/src/include/aide/settingsinterface.hpp
@@ -1,14 +1,29 @@
-#ifndef AIDE_SETTINGSINTERFACE_HPP
-#define AIDE_SETTINGSINTERFACE_HPP
+#ifndef AIDE_SETTINGS_INTERFACE_HPP
+#define AIDE_SETTINGS_INTERFACE_HPP
+
+#include <string>
+
+#include <QVariant>
 
 namespace aide
 {
+    class HierarchicalId;
+
     class SettingsInterface
     {
     public:
         virtual ~SettingsInterface();
 
+        virtual void setValue(const HierarchicalId& group, std::string key,
+                              const QVariant& value) = 0;
+
+        virtual QVariant value(const HierarchicalId& group)    = 0;
+        virtual QVariant value(const HierarchicalId& group,
+                               const QVariant& defaultValue()) = 0;
+
+        virtual void save() = 0;
+        virtual void load() = 0;
     };
 } // namespace aide
 
-#endif // AIDE_SETTINGSINTERFACE_HPP
+#endif // AIDE_SETTINGS_INTERFACE_HPP
