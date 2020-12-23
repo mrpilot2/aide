290fc556ff4536fb12114e84d322659007b0e1d7
Markus Reitboeck <mrpilot@gmx.net>
2020-12-23 23:56:41 +0100

    New translations aide_en.ts (German) [ci skip]

--- aide/src/gui/res/translations/aide_de_DE.ts
+++ aide/src/gui/res/translations/aide_de_DE.ts
@@ -1,68 +1,68 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de" sourcelanguage="en">
-<context>
+  <context>
     <name>MainWindow</name>
     <message>
-        <source>MainWindow</source>
-        <translation>Hauptfenster</translation>
+      <source>MainWindow</source>
+      <translation>Hauptfenster</translation>
     </message>
     <message>
-        <source>File</source>
-        <translation>Datei</translation>
+      <source>File</source>
+      <translation>Datei</translation>
     </message>
     <message>
-        <source>Quit</source>
-        <translation>Beenden</translation>
+      <source>Quit</source>
+      <translation>Beenden</translation>
     </message>
     <message>
-        <source>Help</source>
-        <translation>Hilfe</translation>
+      <source>Help</source>
+      <translation>Hilfe</translation>
     </message>
     <message>
-        <source>About Qt</source>
-        <translation>Über Qt</translation>
+      <source>About Qt</source>
+      <translation>Über Qt</translation>
     </message>
     <message>
-        <source>About aIDE</source>
-        <translation>Über aIDE</translation>
+      <source>About aIDE</source>
+      <translation>Über aIDE</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>QApplication</name>
     <message>
-        <source>Quits the application</source>
-        <translation type="unfinished"></translation>
+      <source>Quits the application</source>
+      <translation type="unfinished">Quits the application</translation>
     </message>
     <message>
-        <source>Quit</source>
-        <translation type="unfinished">Beenden</translation>
+      <source>Quit</source>
+      <translation>Beenden</translation>
     </message>
     <message>
-        <source>About Qt</source>
-        <translation type="unfinished">Über Qt</translation>
+      <source>About Qt</source>
+      <translation>Über Qt</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SearchLineEdit</name>
     <message>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
+      <source>Form</source>
+      <translation type="unfinished">Form</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>aide::gui::MainWindow</name>
     <message>
-        <source>Are you sure you want to exit?</source>
-        <translation type="unfinished"></translation>
+      <source>Are you sure you want to exit?</source>
+      <translation type="unfinished">Are you sure you want to exit?</translation>
     </message>
     <message>
-        <source>Confirm exit</source>
-        <translation type="unfinished"></translation>
+      <source>Confirm exit</source>
+      <translation type="unfinished">Confirm exit</translation>
     </message>
     <message>
-        <source>Don&apos;t ask again</source>
-        <translation type="unfinished"></translation>
+      <source>Don&apos;t ask again</source>
+      <translation type="unfinished">Don&apos;t ask again</translation>
     </message>
-</context>
+  </context>
 </TS>
