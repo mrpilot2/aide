763c67c21e7b6310942afd8248200fb17fd1376a
Markus Reitboeck <mrpilot@gmx.net>
2020-12-23 23:56:10 +0100

    New translations aide_en.ts (Vietnamese) [ci skip]

--- aide/src/gui/res/translations/aide_vi_VN.ts
+++ aide/src/gui/res/translations/aide_vi_VN.ts
@@ -28,4 +28,41 @@
       <translation type="unfinished">About aIDE</translation>
     </message>
   </context>
+  <context>
+    <name>QApplication</name>
+    <message>
+      <source>Quits the application</source>
+      <translation type="unfinished">Quits the application</translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation type="unfinished">Quit</translation>
+    </message>
+    <message>
+      <source>About Qt</source>
+      <translation type="unfinished">About Qt</translation>
+    </message>
+  </context>
+  <context>
+    <name>SearchLineEdit</name>
+    <message>
+      <source>Form</source>
+      <translation type="unfinished">Form</translation>
+    </message>
+  </context>
+  <context>
+    <name>aide::gui::MainWindow</name>
+    <message>
+      <source>Are you sure you want to exit?</source>
+      <translation type="unfinished">Are you sure you want to exit?</translation>
+    </message>
+    <message>
+      <source>Confirm exit</source>
+      <translation type="unfinished">Confirm exit</translation>
+    </message>
+    <message>
+      <source>Don&apos;t ask again</source>
+      <translation type="unfinished">Don&apos;t ask again</translation>
+    </message>
+  </context>
 </TS>
