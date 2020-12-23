1fff5aa979881e0a26891a006037a860698aa0cc
Markus Reitboeck <mrpilot@gmx.net>
2020-12-23 23:56:11 +0100

    New translations aide_en.ts (English) [ci skip]

---
+++ aide/src/gui/res/translations/aide_en_US.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <source>MainWindow</source>
+      <translation type="unfinished">MainWindow</translation>
+    </message>
+    <message>
+      <source>File</source>
+      <translation type="unfinished">File</translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation type="unfinished">Quit</translation>
+    </message>
+    <message>
+      <source>Help</source>
+      <translation type="unfinished">Help</translation>
+    </message>
+    <message>
+      <source>About Qt</source>
+      <translation type="unfinished">About Qt</translation>
+    </message>
+    <message>
+      <source>About aIDE</source>
+      <translation type="unfinished">About aIDE</translation>
+    </message>
+  </context>
+  <context>
+    <name>QApplication</name>
+    <message>
+      <source>Quits the application</source>
+      <translation type="unfinished">Quits the application</translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation type="unfinished">Quit</translation>
+    </message>
+    <message>
+      <source>About Qt</source>
+      <translation type="unfinished">About Qt</translation>
+    </message>
+  </context>
+  <context>
+    <name>SearchLineEdit</name>
+    <message>
+      <source>Form</source>
+      <translation type="unfinished">Form</translation>
+    </message>
+  </context>
+  <context>
+    <name>aide::gui::MainWindow</name>
+    <message>
+      <source>Are you sure you want to exit?</source>
+      <translation type="unfinished">Are you sure you want to exit?</translation>
+    </message>
+    <message>
+      <source>Confirm exit</source>
+      <translation type="unfinished">Confirm exit</translation>
+    </message>
+    <message>
+      <source>Don&apos;t ask again</source>
+      <translation type="unfinished">Don&apos;t ask again</translation>
+    </message>
+  </context>
+</TS>
