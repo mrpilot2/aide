ed1692ddb3510ae5e424764d7dea5ef1304e06f0
Markus Reitboeck <mrpilot@gmx.net>
2021-05-03 14:15:13 +0200

    (+) add custom AideTreeView
    allows to set placeholder text

---
+++ aide/src/gui/widgets/aidetreeview.cpp
@@ -0,0 +1,44 @@
+//
+// Created by markus on 5/3/21.
+//
+
+#include "aidetreeview.hpp"
+
+#include <utility>
+
+#include <QBrush>
+#include <QPainter>
+#include <QPalette>
+
+using aide::widgets::AideTreeView;
+
+AideTreeView::AideTreeView(QWidget* parent)
+    : QTreeView(parent)
+{}
+
+void AideTreeView::paintEvent(QPaintEvent* event)
+{
+    if ((model() != nullptr) && model()->rowCount() > 0) {
+        QTreeView::paintEvent(event);
+    } else {
+        QPainter painter(viewport());
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 12, 0)
+        const auto placeHolderTextAlpha{128};
+        QColor textColor{palette().text().color()};
+        textColor.setAlpha(placeHolderTextAlpha);
+        painter.setPen(textColor);
+#else
+        painter.setPen(palette().placeholderText().color());
+#endif
+
+        QRect textRect = painter.fontMetrics().boundingRect(placeHolderText);
+        textRect.moveCenter(viewport()->rect().center());
+        painter.drawText(textRect, Qt::AlignCenter, placeHolderText);
+    }
+}
+
+void AideTreeView::setPlaceHolderText(QString text)
+{
+    placeHolderText = std::move(text);
+}
---
+++ aide/src/gui/widgets/aidetreeview.hpp
@@ -0,0 +1,24 @@
+
+#ifndef AIDE_AIDE_TREE_VIEW_HPP
+#define AIDE_AIDE_TREE_VIEW_HPP
+
+#include <QTreeView>
+
+namespace aide::widgets
+{
+    class AideTreeView : public QTreeView
+    {
+    public:
+        explicit AideTreeView(QWidget* parent);
+
+        void setPlaceHolderText(QString text);
+
+    protected:
+        void paintEvent(QPaintEvent* event) override;
+
+    private:
+        QString placeHolderText{tr("There are no elements in this view")};
+    };
+} // namespace aide::widgets
+
+#endif // AIDE_AIDE_TREE_VIEW_HPP
