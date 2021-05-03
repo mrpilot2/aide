b89be50f471d89289fb47e78bf68d604eeab511d
Markus Reitboeck <mrpilot@gmx.net>
2021-05-03 15:54:41 +0200

    (*) extract base class TreeModel frmo SettingsPageGroupTreeModel
    this base class can also be used for the KeyMap tree view

--- aide/src/core/settings/settingspagegrouptreemodel.cpp
+++ aide/src/core/settings/settingspagegrouptreemodel.cpp
@@ -5,6 +5,7 @@
 #include "hierarchicalid.hpp"
 #include "settings/settingspage.hpp"
 #include "settings/settingspageregistry.hpp"
+#include "treemodel.hpp"
 
 using aide::core::SettingsPageGroupTreeModel;
 using aide::core::SettingsPagePtr;
@@ -12,9 +13,8 @@ using aide::core::SettingsPageRegistry;
 using aide::core::TreeItemPtr;
 
 SettingsPageGroupTreeModel::SettingsPageGroupTreeModel(QObject* parent)
-    : QAbstractItemModel(parent)
-    , rootItem(std::make_shared<TreeItem>(
-          std::vector<QVariant>({{"Group", "Complete"}})))
+    : TreeModel(parent, std::make_shared<TreeItem>(
+                            std::vector<QVariant>({{"Group", "Complete"}})))
 {
     setupModelData(rootItem);
 }
@@ -55,17 +55,6 @@ void SettingsPageGroupTreeModel::setupModelData(const TreeItemPtr& parent)
     }
 }
 
-QVariant aide::core::SettingsPageGroupTreeModel::headerData(
-    int section, Qt::Orientation orientation, int role) const
-{
-    if (orientation == Qt::Horizontal && role == Qt::DisplayRole &&
-        section == 0) {
-        return rootItem->data(static_cast<size_t>(section));
-    }
-
-    return QVariant();
-}
-
 QVariant SettingsPageGroupTreeModel::data(const QModelIndex& index,
                                           int role) const
 {
@@ -91,58 +80,6 @@ Qt::ItemFlags SettingsPageGroupTreeModel::flags(const QModelIndex& index) const
     if (!index.isValid()) { return Qt::NoItemFlags; }
 
     return QAbstractItemModel::flags(index);
-}
-
-// NOLINTNEXTLINE
-QModelIndex SettingsPageGroupTreeModel::index(int row, int column,
-                                              const QModelIndex& parent) const
-{
-    if (!hasIndex(row, column, parent)) { return QModelIndex(); }
-
-    TreeItem* parentItem{
-        !parent.isValid() ? rootItem.get()
-                          : static_cast<TreeItem*>(parent.internalPointer())};
-
-    auto childItem = parentItem->child(static_cast<size_t>(row));
-
-    if (childItem != nullptr) {
-        return createIndex(row, column, childItem.get());
-    }
-
-    return QModelIndex();
-}
-
-QModelIndex SettingsPageGroupTreeModel::parent(const QModelIndex& index) const
-{
-    if (!index.isValid()) { return QModelIndex(); }
-
-    auto* childItem        = static_cast<TreeItem*>(index.internalPointer());
-    TreeItemPtr parentItem = childItem->parent();
-
-    if (parentItem == rootItem) { return QModelIndex(); }
-
-    return createIndex(static_cast<int>(parentItem->row()), 0,
-                       parentItem.get());
-}
-
-// NOLINTNEXTLINE
-int SettingsPageGroupTreeModel::rowCount(const QModelIndex& parent) const
-{
-    if (parent.column() > 0) { return 0; }
-
-    TreeItem* parentItem{
-        !parent.isValid() ? rootItem.get()
-                          : static_cast<TreeItem*>(parent.internalPointer())};
-
-    return static_cast<int>(parentItem->childCount());
-}
-// NOLINTNEXTLINE
-int SettingsPageGroupTreeModel::columnCount(const QModelIndex& parent) const
-{
-    return !parent.isValid() ? static_cast<int>(rootItem->columnCount())
-                             : static_cast<int>(static_cast<TreeItem*>(
-                                                    parent.internalPointer())
-                                                    ->columnCount());
 }
 
 SettingsPagePtr SettingsPageGroupTreeModel::findCorrespondingSettingsPage(
