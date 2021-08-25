31d8ee38f55967675c5817f475ee869005b4d5f4
Markus Reitboeck <mrpilot@gmx.net>
2021-08-25 22:19:40 +0200

    (+) initial AddShortcutDialog

---
+++ aide/src/gui/settings/keymap/addshortcutdialog.hpp
@@ -0,0 +1,27 @@
+
+#ifndef AIDE_ADD_SHORTCUT_DIALOG_HPP
+#define AIDE_ADD_SHORTCUT_DIALOG_HPP
+
+#include <memory>
+
+#include <QDialog>
+
+namespace Ui
+{
+    class AddShortcutDialog;
+} // namespace Ui
+
+namespace aide::gui
+{
+    class AddShortcutDialog : public QDialog
+    {
+    public:
+        explicit AddShortcutDialog(QWidget* parent = nullptr);
+        ~AddShortcutDialog() override;
+
+    private:
+        std::unique_ptr<Ui::AddShortcutDialog> ui;
+    };
+} // namespace aide::gui
+
+#endif // AIDE_ADD_SHORTCUT_DIALOG_HPP
