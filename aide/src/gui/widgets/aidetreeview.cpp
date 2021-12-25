4c6c6dfe2c4dad4ca1ed0768b4986bf870b22cb7
Markus Reitboeck <mrpilot@gmx.net>
2021-12-25 14:14:43 +0100

    (*) improve AideTreeView placeholder
    if view is empty due to active filter append this info to the
    placeholder text

--- aide/src/gui/widgets/aidetreeview.cpp
+++ aide/src/gui/widgets/aidetreeview.cpp
@@ -10,6 +10,7 @@
 #include <QBrush>
 #include <QPainter>
 #include <QPalette>
+#include <QSortFilterProxyModel>
 
 using aide::widgets::AideTreeView;
 
@@ -21,24 +22,38 @@ AideTreeView::AideTreeView(QWidget* parent)
 
 void AideTreeView::paintEvent(QPaintEvent* event)
 {
+    QString textToDisplay(placeHolderText);
     if ((model() != nullptr) && model()->rowCount() > 0) {
         QTreeView::paintEvent(event);
-    } else {
-        QPainter painter(viewport());
+        return;
+    }
+
+    auto const* sourceModel = model();
+    if (auto const* filterModel = qobject_cast<QSortFilterProxyModel*>(model());
+        filterModel != nullptr) {
+        sourceModel = filterModel->sourceModel();
+    }
+
+    if (sourceModel != nullptr && model() != nullptr &&
+        (model()->rowCount() != sourceModel->rowCount())) {
+        textToDisplay.append(
+            QApplication::translate("AideTableView", " due to active filter"));
+    }
+
+    QPainter painter(viewport());
 
 #if QT_VERSION < QT_VERSION_CHECK(5, 12, 0)
-        const auto placeHolderTextAlpha{128};
-        QColor textColor{palette().text().color()};
-        textColor.setAlpha(placeHolderTextAlpha);
-        painter.setPen(textColor);
+    const auto placeHolderTextAlpha{128};
+    QColor textColor{palette().text().color()};
+    textColor.setAlpha(placeHolderTextAlpha);
+    painter.setPen(textColor);
 #else
-        painter.setPen(palette().placeholderText().color());
+    painter.setPen(palette().placeholderText().color());
 #endif
 
-        QRect textRect = painter.fontMetrics().boundingRect(placeHolderText);
-        textRect.moveCenter(viewport()->rect().center());
-        painter.drawText(textRect, Qt::AlignCenter, placeHolderText);
-    }
+    QRect textRect = painter.fontMetrics().boundingRect(placeHolderText);
+    textRect.moveCenter(viewport()->rect().center());
+    painter.drawText(textRect, Qt::AlignCenter, placeHolderText);
 }
 
 void AideTreeView::setPlaceHolderText(QString text)
