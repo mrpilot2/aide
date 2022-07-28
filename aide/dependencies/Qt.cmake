222ec495a2c1eb3921ab17a4d70aca32d35f9f39
Markus Reitboeck <mrpilot@gmx.net>
2022-07-28 22:06:04 +0200

    (*) find Qt5 only if it does not exist yet
    otherwise there is an error if the library is added
    with FetchContent

--- aide/dependencies/Qt.cmake
+++ aide/dependencies/Qt.cmake
@@ -13,23 +13,25 @@ if(TARGET Qt6::Core)
 else()
   message(STATUS "Using Qt5")
 
-  find_package(
-    Qt5
-    COMPONENTS Core Widgets
-    REQUIRED
-  )
-
-  if(TARGET Qt5::Core)
-    set_target_properties(
-      Qt5::Core Qt5::Widgets Qt5::moc Qt5::uic Qt5::rcc
-      PROPERTIES IMPORTED_GLOBAL TRUE
+  if(NOT TARGET Qt5::Core)
+    find_package(
+      Qt5
+      COMPONENTS Core Widgets
+      REQUIRED
     )
-    if(NOT TARGET Qt::Core)
-      add_library(Qt::Core ALIAS Qt5::Core)
-      add_library(Qt::Widgets ALIAS Qt5::Widgets)
-      add_executable(Qt::moc ALIAS Qt5::moc)
-      add_executable(Qt::uic ALIAS Qt5::uic)
-      add_executable(Qt::rcc ALIAS Qt5::rcc)
+
+    if(TARGET Qt5::Core)
+      set_target_properties(
+        Qt5::Core Qt5::Widgets Qt5::moc Qt5::uic Qt5::rcc
+        PROPERTIES IMPORTED_GLOBAL TRUE
+      )
+      if(NOT TARGET Qt::Core)
+        add_library(Qt::Core ALIAS Qt5::Core)
+        add_library(Qt::Widgets ALIAS Qt5::Widgets)
+        add_executable(Qt::moc ALIAS Qt5::moc)
+        add_executable(Qt::uic ALIAS Qt5::uic)
+        add_executable(Qt::rcc ALIAS Qt5::rcc)
+      endif()
     endif()
   endif()
 
