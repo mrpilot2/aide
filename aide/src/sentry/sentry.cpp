d442bd1565ae0fd8284bc507fac798f79d54524b
Markus Reitboeck <mrpilot@gmx.net>
2023-01-14 23:16:33 +0100

    feat(Sentry): add user information

--- aide/src/sentry/sentry.cpp
+++ aide/src/sentry/sentry.cpp
@@ -3,6 +3,8 @@
 
 #include <sentry.h>
 
+#include <QProcess>
+
 #include <aide/buildinformation.hpp>
 
 using aide::Sentry;
@@ -15,6 +17,23 @@ namespace
         return config.applicationName + "@" + config.applicationVersion;
     }
 
+    std::string getUserName()
+    {
+        QString name = qgetenv("USER");
+        if (name.isEmpty()) { name = qgetenv("USERNAME"); }
+        return name.toStdString();
+    }
+
+    std::string getUserId()
+    {
+        QProcess process;
+        process.setProgram("id");
+        process.setArguments({"-u"});
+        process.start();
+        while (process.state() != QProcess::NotRunning)
+            qApp->processEvents();
+        return process.readAll().toStdString();
+    }
 } // namespace
 
 void Sentry::initializeConnection(const SentryConfig& config)
@@ -29,9 +48,26 @@ void Sentry::initializeConnection(const SentryConfig& config)
 
     sentry_init(options);
 
+    sentry_value_t user = sentry_value_new_object();
+    sentry_value_set_by_key(user, "ip_address",
+                            sentry_value_new_string("{{auto}}"));
+    sentry_value_set_by_key(user, "username",
+                            sentry_value_new_string(::getUserName().c_str()));
+    sentry_value_set_by_key(user, "id",
+                            sentry_value_new_string(::getUserId().c_str()));
+
+    sentry_set_user(user);
+
     sentry_set_tag("qt.version", QT_VERSION_STR);
     sentry_set_tag("aide.version",
                    aide::build_information::AIDE_VERSION_STRING);
+
+    sentry_value_t event = sentry_value_new_message_event(
+        /*   level */ SENTRY_LEVEL_INFO,
+        /*  logger */ "custom",
+        /* message */ "It works!");
+
+    sentry_capture_event(event);
 }
 
 void Sentry::closeConnection()
