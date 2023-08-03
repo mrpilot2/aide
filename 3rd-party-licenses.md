92977b4c4b26b97aba50f1473ebe89f3cd83b2f7
Markus Reitboeck <mrpilot@gmx.net>
2023-08-03 23:05:51 +0200

    docs: update third party licenses

--- 3rd-party-licenses.md
+++ 3rd-party-licenses.md
@@ -1,16 +1,34 @@
 ## Libraries directly included in aIDE
 
-### [fmt](https://github.com/fmtlib/fmt/)
+### [abi-compliance-checker](https://github.com/lvc/abi-compliance-checker)
 
-Version: 7.1.3
+Version: Undefined
 
-License: [MIT (custom)](https://github.com/fmtlib/fmt/blob/7.1.3/LICENSE.rst)
+License: [LGPL-2.1](https://github.com/lvc/abi-compliance-checker/blob/master/LICENSE)
+
+### [cmake-abicc](https://github.com/Oleh-Kravchenko/cmake-abicc)
+
+Version: None
+
+License: [BSD 3-Clause License](https://github.com/Oleh-Kravchenko/cmake-abicc/blob/master/LICENSE)
 
 ### [catch2](https://github.com/catchorg/Catch2/)
 
 Version: 2.13.6
 
 License: [Boost Software License 1.0](https://github.com/catchorg/Catch2/blob/v2.13.6/LICENSE.txt)
+
+### [fmt](https://github.com/fmtlib/fmt/)
+
+Version: 7.1.3
+
+License: [MIT (custom)](https://github.com/fmtlib/fmt/blob/7.1.3/LICENSE.rst)
+
+### [libyang (parts used for ABI check)](https://github.com/CESNET/libyang/blob/master/CMakeModules/ABICheck.cmake)
+
+Version: None
+
+License: [BSD 3-Clause License](https://github.com/CESNET/libyang/blob/master/LICENSE)
 
 ### [Qt](https://www.qt.io/)
 
--- aide/dependencies/addlicensedependency.cmake
+++ aide/dependencies/addlicensedependency.cmake
@@ -1,18 +1,20 @@
 macro(add_license_dependency)
-  set(noValueArgs "")
+  set(noValueArgs MARKDOWN_ONLY)
   set(oneValueArgs PROJECT PROJECT_URL VERSION LICENSE LICENSE_URL)
   set(multiValueArgs "")
   cmake_parse_arguments(
     ALD "${noValueArgs}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN}
   )
 
-  set(table_content
-      "${table_content}<tr valign=\"top\"><td class=\"firstColumn\"><a class=\"name\"
-  href=\"${ALD_PROJECT_URL}\">${ALD_PROJECT}</a> <span class=\"version\">${ALD_VERSION}</span></td><td
-  class=\"secondColumn\"><a
-  class=\"license\"
-  href=\"${ALD_LICENSE_URL}\">${ALD_LICENSE}</a></td></tr>"
-  )
+  if(NOT ALD_MARKDOWN_ONLY)
+    set(table_content
+        "${table_content}<tr valign=\"top\"><td class=\"firstColumn\"><a class=\"name\"
+    href=\"${ALD_PROJECT_URL}\">${ALD_PROJECT}</a> <span class=\"version\">${ALD_VERSION}</span></td><td
+    class=\"secondColumn\"><a
+    class=\"license\"
+    href=\"${ALD_LICENSE_URL}\">${ALD_LICENSE}</a></td></tr>"
+    )
+  endif()
 
   set(markdown_content
       "${markdown_content}### [${ALD_PROJECT}](${ALD_PROJECT_URL})\n\nVersion: ${ALD_VERSION}\n\nLicense: [${ALD_LICENSE}](${ALD_LICENSE_URL})\n\n"
