2d1a44bd3cd46c0c190c06cedb8577659b2ab41d
Markus Reitböck <mrpilot@gmx.net>
2026-04-05 17:43:28 +0200

    chore: upgrade clang-format

--- aide/src/utils/systemmemory_lin.cpp
+++ aide/src/utils/systemmemory_lin.cpp
@@ -6,9 +6,7 @@ using aide::utils::SystemMemory;
 
 std::optional<uint64_t> SystemMemory::getAvailableRAMInBytes()
 {
-    struct sysinfo info
-    {
-    };
+    struct sysinfo info{};
     if (sysinfo(&info) == -1) { return std::nullopt; }
     return info.totalram;
 }
